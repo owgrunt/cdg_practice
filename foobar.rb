cc88167613ba25ea146b47ee8fc1992cfb015241
Artem Samarsky <artem.samarsky@gmail.com>
2022-11-23 17:40:29 +0100

    Updating foobar

--- foobar.rb
+++ foobar.rb
@@ -1,1 +1,12 @@
-aaa
+print "Enter the first number > "
+firstNumber = gets.to_i
+
+print "Enter the second number > "
+secondNumber = gets.to_i
+
+def foobar(firstNumber, secondNumber)
+    firstNumber == 20 || secondNumber == 20 ? result = secondNumber : result = firstNumber + secondNumber
+    puts result
+end
+
+foobar(firstNumber, secondNumber)
